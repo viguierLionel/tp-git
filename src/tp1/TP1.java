e95a1e64cdef9ee673eb29ed4d7c1bacab9a2ca9
lvigui03 <lvigui03@pcalb-mm0605.univ-jfc.fr>
2019-09-17 14:46:32 +0200

    jaime le java

--- src/tp1/TP1.java
+++ src/tp1/TP1.java
@@ -15,6 +15,7 @@ public class TP1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hi");
         // TODO code application logic here
         //beans
         //beans
