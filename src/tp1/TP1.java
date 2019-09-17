83cc481778a5cbae9e1d98db581c87ff1c65a813
mhoeke <mhoeke@pcalb-mm0606.univ-jfc.fr>
2019-09-17 14:47:43 +0200

    change

--- src/tp1/TP1.java
+++ src/tp1/TP1.java
@@ -15,7 +15,7 @@ public class TP1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hi46565465");
         // TODO code application logic here
         //beans
         //beans
